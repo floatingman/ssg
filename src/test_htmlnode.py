7a6829c2e2e0d049abe6c11d0b8f59818e6c1789
floatingman <dan@danlovesprogramming.com>
2024-10-11 12:33:46 -0500

    Added LeafNode Child class to HTMLNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -23,3 +23,14 @@ class TestHTMLNode(unittest.TestCase):
     def test_none_props_to_html(self):
         node = HTMLNode("div", None, None, None)
         self.assertEqual(node.props_to_html(), None)
+
+    def test_to_html_with_no_value(self):
+        node = LeafNode("div", None, None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_with_no_tag(self):
+        node = LeafNode(None, "This is a text node", None)
+        self.assertEqual(node.to_html(), "This is a text node")
+
+if __name__ == "__main__":
+    unittest.main()
