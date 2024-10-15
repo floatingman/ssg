361b66e29a34faa29061b2a48951a938be0b8a17
floatingman <dan@danlovesprogramming.com>
2024-10-15 18:23:30 -0500

    Refactored classes, added function to split nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,10 +6,10 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError("to_html not implemented")
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
         props_html = ""
         for prop in self.props:
@@ -17,7 +17,7 @@ class HTMLNode:
         return props_html
 
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
@@ -26,7 +26,7 @@ class LeafNode(HTMLNode):
 
     def to_html(self):
         if self.value is None:
-            raise ValueError("value cannot be None")
+            raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
@@ -40,35 +40,14 @@ class ParentNode(HTMLNode):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if self.children is None:
-            raise ValueError("children cannot be None")
         if self.tag is None:
-            raise ValueError("tag cannot be None")
-
-        html = f"<{self.tag}>"
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
         for child in self.children:
-            html += child.to_html()
-        html += f"</{self.tag}>"
-
-        return html
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
 
     def __repr__(self):
-        return f"ParentNode({self.tag}, {self.children}, {self.props})"
-
-
-def text_node_to_html(text_node):
-    match text_node.text_type:
-        case "text":
-            return LeafNode(None, text_node.text)
-        case "bold":
-            return LeafNode("b", text_node.text)
-        case "italic":
-            return LeafNode("i", text_node.text)
-        case "code":
-            return LeafNode("code", text_node.text)
-        case "link":
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        case "image":
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-        case _:
-            raise ValueError(f"Unknown text type: {text_node.text_type}")
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,31 +1,19 @@
 import unittest
-
-from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html
-from textnode import TextNode
+from htmlnode import LeafNode, ParentNode, HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-
-    def test_repr(self):
-        node = HTMLNode("div", None, None, {"class": "my-class", "id": "my-id"})
-        self.assertEqual(
-            node.__repr__(),
-            "HTMLNode(div, None, None, {'class': 'my-class', 'id': 'my-id'})",
-        )
-
-    def test_to_html(self):
-        node = HTMLNode("div", None, None, {"class": "my-class", "id": "my-id"})
-        self.assertRaises(NotImplementedError, node.to_html)
-
-    def test_props_to_html(self):
+    def test_to_html_props(self):
         node = HTMLNode(
-            "div", "Hello, world!", None, {"class": "my-class", "id": "my-id"}
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
         )
-        self.assertEqual(node.props_to_html(), f' class="my-class" id="my-id"')
-
-    def test_blank_props_to_html(self):
-        node = HTMLNode("div", None, None, None)
-        self.assertEqual(node.props_to_html(), "")
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
 
     def test_values(self):
         node = HTMLNode(
@@ -49,67 +37,25 @@ class TestHTMLNode(unittest.TestCase):
             None,
         )
 
-    def test_leafnode_child_class_of_htmlnode(self):
-        node = LeafNode("div", None, None)
-        assert issubclass(type(node), HTMLNode)
-
-    def test_to_html_with_no_value(self):
-        node = LeafNode("div", None, None)
-        self.assertRaises(ValueError, node.to_html)
-
-    def test_to_html_with_no_tag(self):
-        node = LeafNode(None, "This is a text node", None)
-        self.assertEqual(node.to_html(), "This is a text node")
-
-    def test_parentnode_child_class_of_htmlnode(self):
-        node = ParentNode(
+    def test_repr(self):
+        node = HTMLNode(
             "p",
-            [
-                LeafNode("b", "Bold text"),
-                LeafNode(None, "Normal text"),
-                LeafNode("i", "italic text"),
-                LeafNode(None, "Normal text"),
-            ],
-            {"href": "https://www.goot.dev"},
-        )
-        assert issubclass(type(node), HTMLNode)
-
-    def test_parentnode_children_argument_not_optional(self):
-        node = ParentNode(
-            "p",
+            "What a strange world",
             None,
-            {"href": "https://www.goot.dev"},
-        )
-        self.assertRaisesRegex(ValueError, "children cannot be None", node.to_html)
-
-    def test_parentnode_requires_tag_argument(self):
-        node = ParentNode(
-            None,
-            [
-                LeafNode("b", "Bold text"),
-                LeafNode(None, "Normal text"),
-                LeafNode("i", "italic text"),
-                LeafNode(None, "Normal text"),
-            ],
-            {"href": "https://www.goot.dev"},
-        )
-        self.assertRaisesRegex(ValueError, "tag cannot be None", node.to_html)
-
-    def test_parentnode_to_html(self):
-        node = ParentNode(
-            "p",
-            [
-                LeafNode("b", "Bold text"),
-                LeafNode(None, "Normal text"),
-                LeafNode("i", "italic text"),
-                LeafNode(None, "Normal text"),
-            ],
-            {"href": "https://www.goot.dev"},
+            {"class": "primary"},
         )
         self.assertEqual(
-            node.to_html(),
-            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
     def test_to_html_with_children(self):
         child_node = LeafNode("span", "child")
@@ -153,23 +99,6 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.to_html(),
             "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
-        )
-
-    def test_convert_text_node_to_html(self):
-        node = TextNode("This is a text node", "bold")
-        leaf_node = text_node_to_html(node)
-        self.assertEqual(leaf_node.tag, "b")
-        self.assertEqual(leaf_node.value, "This is a text node")
-        self.assertEqual(leaf_node.to_html(), "<b>This is a text node</b>")
-
-    def test_convert_text_node_to_html_with_url(self):
-        node = TextNode("This is a text node", "link", "https://example.com")
-        leaf_node = text_node_to_html(node)
-        self.assertEqual(leaf_node.tag, "a")
-        self.assertEqual(leaf_node.value, "This is a text node")
-        self.assertEqual(leaf_node.props["href"], "https://example.com")
-        self.assertEqual(
-            leaf_node.to_html(), '<a href="https://example.com">This is a text node</a>'
         )
 
 
